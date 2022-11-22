13080d050e7c8749ccaae9558006b4cb50511cd9
Administrator <https://github.com/shishanwangzhen/corewell-study.git>
2022-11-22 12:09:32 +0800

    优化入参

--- src/main/java/com/corewell/study/controller/ProjectController.java
+++ src/main/java/com/corewell/study/controller/ProjectController.java
@@ -7,10 +7,7 @@ import com.corewell.study.domain.result.ResultMsg;
 import com.corewell.study.service.ProjectService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
