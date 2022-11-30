91727597bff9aa7e40988ee6c31537b950691c58
Administrator <https://github.com/shishanwangzhen/corewell-study.git>
2022-11-30 09:56:49 +0800

    配置Redis

---
+++ src/main/java/com/corewell/study/constants/BaseRedisKeyConstants.java
@@ -0,0 +1,15 @@
+package com.corewell.study.constants;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: wangzhen
+ * @Date: 2022/11/30/9:51
+ * @Description:
+ */
+public class BaseRedisKeyConstants {
+    /**
+     * Redis普通键前缀
+     * */
+    public static final String redis_key_pre="core:study:access:";
+}
